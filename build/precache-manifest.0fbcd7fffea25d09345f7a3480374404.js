0af13ac819969562ed9a0427ea950afd3bfa320f
Mark Fasel <markfasel@gmail.com>
2019-03-14 13:11:57 -0500

    init build

---
+++ build/precache-manifest.0fbcd7fffea25d09345f7a3480374404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eea9343c0a7c0162b5cd",
+    "url": "/static/css/main.69df0a16.chunk.css"
+  },
+  {
+    "revision": "eea9343c0a7c0162b5cd",
+    "url": "/static/js/main.00013580.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fd49720bdd6064179e65c29581a13192",
+    "url": "/index.html"
+  }
+];
