42d75adc02002d6daa7e09178d8858fc423df851
Mark Fasel <markfasel@gmail.com>
2022-08-02 13:09:35 -0400

    update about section

--- src/components/structure/About.js
+++ src/components/structure/About.js
@@ -14,13 +14,13 @@ class About extends Component {
           </div>
           <div className="about-left">
             <div className="content">
-              <h2>Hi there! I’m Mark, UX designer and software developer. I love helping clients dream big but create the impossible. I help solve problems, create solutions, and build brands.</h2>
-              <p>I’ve been doing this for over 14 years and have really developed a passion and love for this sort of work. I build crazy cool websites, mobile apps, and web apps. I love modern technologies such as React and Angular, PHP & Laravel and Ruby on Rails mixed with a variety of other backend tools and database solutions.</p>
+              <h2>Hi there! I’m Mark, a Sr. Software Engineer with a focus on front-end development and UX design. I am currently working at Scorpion where I get to lead projects, be a mentor, and build amazing products for our customers. I love helping clients dream big but create the impossible. I help solve problems, create solutions, and build brands.</h2>
+              <p>I’ve been doing this for 10+ years and have a passion and love for this work. I build crazy cool websites, mobile apps, and web apps. I love modern technologies such as React and Angular, PHP & Laravel and Ruby on Rails mixed with a variety of other backend tools and database solutions.</p>
             </div>
           </div>
           <div className="about-right organge-block">
             <div className="content">
-              <p>In the past, I have worked for large organizations and small agencies doing a variety of development, from <a href="https://www.hopkinsallchildrens.org" name="Children's hospital in St. Petersburg, Florida">Johns Hopkins All Children's Hospital</a>, <a href="https://www.daveramsey.com" name="Delivering financial hope to others thru Total Money Makeover and Financial Peace">Dave Ramsey</a>, and <a href="https://www.differential.com" name="A small strategic web agency that delivers custom web applications located in Cincinati, Ohio">Differential</a>.</p>
+              <p>In the past, I have worked for large organizations and small agencies doing a variety of development, from <a href="https://publix.com" name="Publix Supermarkets Inc">Publix Supermarkets, Inc</a> as a Software Engineer, <a href="https://www.hopkinsallchildrens.org" name="Children\'s hospital in St. Petersburg, Florida">Johns Hopkins All Children\'s Hospital</a>, <a href="https://www.daveramsey.com" name="Delivering financial hope to others thru Total Money Makeover and Financial Peace">Dave Ramsey</a>, and <a href="https://www.differential.com" name="A small strategic web agency that delivers custom web applications located in Cincinati, Ohio">Differential</a>.</p>
               <p>From WordPress and Shopify to custom web apps, extensive UX and frontend development and full stack programming, I love it all and love to build partnerships with each client!</p>
               <p><a href="/#" className="resume-button">Check out my resume!</a></p>
             </div>
